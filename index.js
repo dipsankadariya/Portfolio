755ad556aa42ee970bbe537a7e3a2f21cd75b4ca
Dipsan Kadariya <139572157+dipsankadariya@users.noreply.github.com>
2024-02-24 15:18:57 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+
+
+const navElements = document.querySelectorAll('.nav-elements');
+  
+navElements.forEach(navElement => {
+  navElement.addEventListener('click', event => {
+    event.preventDefault();
+alert(`Under construction.
+Will be updated shortly.
+Thank you`);
+  });
+});
+
+const clonedProjects = document.querySelectorAll('.cloned-projects');
+
+clonedProjects.forEach(clonedProjects => {
+  clonedProjects.addEventListener('click',event=>
+  {
+event.preventDefault();
+alert(`Under construction.
+Will be updated shortly.
+Thank you`);
+  });
+});
+
+const webProjects = document.querySelectorAll('.web-projects');
+
+webProjects.forEach(webProjects =>{
+  webProjects.addEventListener('click',event =>
+  {
+    event.preventDefault();
+alert(`Under construction.
+Will be updated shortly.
+Thank you`);
+  });
+});
+
+const minProject=document.querySelectorAll('.min-project');
+
+minProject.forEach(minProject =>{
+  minProject.addEventListener('click',event=>
+  {
+alert(`Under construction.
+Will be updated shortly.
+Thank you`);  
+  });
+});
+
+
+
